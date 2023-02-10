713b847ba9dfe53ff72a69b07180966b2b1ca928
SolarDangelion <wogoj39357@invodua.com>
2023-02-10 20:38:57 +0300

    second

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React, { useState } from 'react';
+//import ReactDOM from 'react-dom/client';
+//import App from './App'; 
+import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const BookSearch = () => {
+  const [query, setQuery] = useState('');
+  const [books, setBooks] = useState([]);
   
+  const handleSearch = (e) => {e.preventDefault();
+    axios
+      .get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+      .then((res) => {setBooks(res.data.items);})
+      .catch((err) => {console.error(err);
+      });
+  };
 
-  
-);
+  return (
+    <div>
+      <form onSubmit = {handleSearch}>
+        <input type = "text" value = {query} onChange = {(e) => setQuery(e.target.value)}/>
+        <button type = "submit">Search</button>
+      </form>
+      {books.map((book) => (<div key={book.id}>
+          <h3>{book.volumeInfo.title}</h3>
+          <p>{book.volumeInfo.authors.join(', ')}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BookSearch;
 
 
